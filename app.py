5d68ec0b9d3a398b93ce76a1f6814a3e11479243
Rony032 <diegovillalobos9@gmail.com>
2025-09-21 20:06:31 -0600

    Update grafico

--- app.py
+++ app.py
@@ -26,3 +26,20 @@ if hist_button:
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
     st.plotly_chart(fig, use_container_width=True)
+
+
+dispersion_button = st.button('Construir diagrama de dispersion')
+
+if dispersion_button:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación de un diagrama de dispersion')
+    # Crear un scatter plot utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de scatter
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    st.plotly_chart(fig, use_container_width=True)
